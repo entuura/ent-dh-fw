a6e6cbfb4cec9ce9c95fc5d4d9eafe41eadec474
whotopia <whotopia@gmail.com>
2017-08-16 13:43:49 +0300

    fixes to fw update script

--- scripts/fw-update.sh
+++ scripts/fw-update.sh
@@ -1,11 +1,18 @@
 #!/bin/sh
 
-#opkg update > /dev/null 2>&1 ; opkg list-upgradable|grep -v Multiple|cut -d ' ' -f 1|xargs opkg upgrade > /dev/null  2>&1
+SPATH=/root/ent-dh-fw/scripts
+ROOTCRON=/etc/crontabs/root
+FILEN='fw-update.sh'
+
+if ! grep -q "$FILEN" $ROOTCRON > /dev/null
+then 
+  echo "0 22 * * * $SPATH/$FILEN" >> $ROOTCRON
+  /etc/init.d/cron enable
+  /etc/init.d/cron start
+fi
 
 opkg update > /dev/null 2>&1
-
 PKGS=`opkg list-installed | awk '{print $1}' | sed ':M;N;$!bM;s#\n# #g'`
-
 for i in $PKGS
 do
    # echo Checking and upgrading package $i as required. 
